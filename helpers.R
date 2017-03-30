5955f1ae4ac2079e8fe2d806c83678afd77bb591
Trybnetic <marc.weitz@onlinehome.de>
2017-03-30 14:14:43 +0200

    adds functionality for the cross validation

--- helpers.R
+++ helpers.R
@@ -2,21 +2,27 @@
 # all packages it depends on
 library(ggplot2)
 
-simulateData <- function(Sample, Noise=1, Model=NA){
+simulateData <- function(Sample, Noise = 1, Model = NA){
   # generate the x predictor
-  x <- runif(Sample,-2,2)
+  x <- round(runif(Sample, -2, 2),2)
+  # add minimal and maximal values for x
+  x <- c(-2, x[2:(Sample -1)], 2)
   # generate the y response
-  y <- 2*x^3 + x^2 - 2*x +5 + rnorm(Sample, sd = Noise)
-  data.frame(x=x, y=y)
+  y <- 2 * x^3 + x^2 - 2 * x +5 + rnorm(length(x), sd = Noise)
+  data.frame(x = x, y = y)
 }
 
-createNewDat <- function(Data, max.poly){
+createNewDat <- function(Data, max.poly, boundary=NA){
   # cretaing data.frame which will store model predictions
 
-  x.new <- seq(min(Data$x), max(Data$x), by=0.05)
-  degree <- rep(1:max.poly, each=length(x.new))
-  predicted <- numeric(length=length(x.new)*max.poly)
-  new.dat <- data.frame(x=rep(x.new, times=max.poly),
+  if (any(is.na(boundary))) {
+    x.new <- seq(min(Data$x), max(Data$x), by = 0.01)
+  } else {
+    x.new <- seq(boundary[1], boundary[2], by = 0.01)
+  }
+  degree <- rep(1:max.poly, each = length(x.new))
+  predicted <- numeric(length = length(x.new) * max.poly)
+  new.dat <- data.frame(x = rep(x.new, times = max.poly),
                         degree,
                         predicted)
   new.dat
@@ -25,17 +31,19 @@ createNewDat <- function(Data, max.poly){
 # fitting lm() polynomials of increasing complexity
 # (up to max.degree) and storing their predictions
 # in the new.dat data.frame
-fitModels <- function(Data, max.poly){
-  new.dat <- createNewDat(Data, max.poly)
+fitModels <- function(Data, max.poly, boundary=NA) {
+  new.dat <- createNewDat(Data, max.poly, boundary=boundary)
 
+  if (any(is.na(boundary))) {
+    x.new <- seq(min(Data$x), max(Data$x), by = 0.01)
+  } else {
+    x.new <- seq(boundary[1], boundary[2], by = 0.01)
+  }
 
-  x.new <- seq(min(Data$x), max(Data$x), by = 0.05)
-
-  for(i in 1:max.poly)
-  {
-    sub.dat <- new.dat[new.dat$degree==i,]
-    new.dat[new.dat$degree==i,3] <- predict(lm(y~poly(x, i), data=Data),
-                                            newdata=data.frame(x=x.new)
+  for (i in 1:max.poly) {
+    sub.dat <- new.dat[new.dat$degree == i, ]
+    new.dat[new.dat$degree == i, 3] <- predict(lm(y~poly(x, i, simple=TRUE), data = Data),
+                                            newdata = data.frame(x = x.new)
                                             )
   }
   new.dat
@@ -45,19 +53,81 @@ plotModels <- function(Data, max.poly){
   new.dat <- fitModels(Data, max.poly)
   # plotting the data and the fitted models
   p <- ggplot()
-  p <- p + geom_point(aes(x, y), Data, colour="darkgrey")
+  p <- p + geom_point(aes(x, y), Data, colour = "darkgrey")
   p <- p + geom_line(aes(x, predicted,
-                    colour=as.factor(degree)),
+                    colour = as.factor(degree)),
                 new.dat)
   p <- p + scale_colour_discrete(name = "Degree")
   p
 }
 
-validate <- function(data, n_bins) {
+
+add_bins <- function(data, n_bins, seed) {
   n <- nrow(data)
   bins <- 1:n_bins
+  k <- ceiling(n / n_bins)
 
-  k = ceiling(n / n_bins)
-
+  set.seed(seed)
   data$bin <- sample(rep(bins, k)[1:n])
+
+  return(data)
+}
+
+
+split_data <- function(data, bin) {
+  list(train = data[data$bin != bin, ],
+       test = data[data$bin == bin, ])
+}
+
+estimate <- function(model, x) {
+  x <- as.integer((2 + x) * 100) + 1
+  return(model$predicted[x])
+}
+
+#' Function to cross validate a dataset
+#'
+#' @param data data.frame of x and y values, x values have to be divisible by .01
+#' @param n_bins number of bins you want to split the data for cross validation
+#' @param max.poly maximum degree of polynomial you want to validate
+#' @param seed seed to control randomness
+#'
+#' @returns data.frame of all points of data with estimated y values
+#' for each polynomial degree and the calculated MSEs
+validate <- function(data, n_bins, max.poly = 2, seed = 1337) {
+  data <- add_bins(data, n_bins, seed)
+  result <- data.frame()
+
+  for (bin in 1:n_bins) {
+    split <- split_data(data, bin)
+
+    model <- fitModels(split$train, max.poly, boundary=c(-2,2))
+    test_df <- split$test
+    test_df <- test_df[order(test_df$x),]
+    rownames(test_df) <- 1:nrow(test_df)
+
+    for (i in 1:max.poly) {
+      sub.dat <- model[model$degree == i, ]
+
+      new.dat <- test_df
+      new.dat$estimate <- estimate(sub.dat, new.dat$x)
+      new.dat$degree <- i
+
+      result <- rbind(result, new.dat)
+    }
+  }
+
+  result$mse <- (result$y - result$estimate)^2
+
+  return(result[order(result$degree),])
+}
+
+validation_se <- function(data, n_bins, max.poly = 2, seed = 1337) {
+  result <- validate(data, n_bins, max.poly=max.poly, seed=seed)
+
+  res <- aggregate(cbind(estimate, mse) ~ degree, result, mean)
+  res$n <- aggregate(mse ~ degree, result, length)$mse
+
+  res$se <- sqrt(res$mse) / sqrt(res$n)
+
+  return(res)
 }
