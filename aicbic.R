a393807d2685b5efd713afc5d7c7adebd6adcf80
rpaul23 <jott.ess@gmx.net>
2017-03-31 20:39:11 +0200

    adds styling on aicbic and cross-validation plots

--- aicbic.R
+++ aicbic.R
@@ -16,11 +16,28 @@ calc_aic_bic <- function(max.poly, data) {
 # plot AIC + BIC
 plot_aic_bic <- function(data) {
   p <- ggplot(data=data, aes(x=degree, y=value, group=measure, colour=measure))
-  p <- p + geom_line()
-  p <- p + geom_point()
+  p <- p + geom_line(size=.8)
+  p <- p + geom_point(size=3)
+  p <- p + ggtitle(label="AIC and BIC")
   p <- p + xlab("Number of polynomials in the model") + labs(colour="Criterion")
-  p <- p + theme(axis.title.x = element_text(),
-                 axis.title.y = element_blank())
-  p <- p + scale_x_continuous(breaks=seq(1,10,1))
+  p <- p + theme_bw()
+  p <- p + theme(panel.background=element_rect(fill="#F0F0F0"), # Set the entire chart region to a light gray color
+                 plot.background=element_rect(fill="#F0F0F0"),
+                 panel.grid.major=element_line(colour="#D0D0D0",size=.5), # Format the grid
+                 panel.border=element_rect(colour="#F0F0F0"),
+                 #panel.border = element_rect(size=1.1, colour="#535353", fill=NA),
+                 axis.ticks=element_blank(),
+                 axis.text.x = element_text(), 
+                 axis.text.y = element_blank(), # Drop y axis text
+                 axis.title.x = element_text(size = 15,colour = "#535353"), # Color axis titles
+                 axis.title.y = element_blank(),
+                 legend.background = element_rect(fill="#F0F0F0"), # Work legend
+                 legend.title = element_text(size=14, colour="#535353"),
+                 legend.text = element_text(size=14, colour="#535353"),
+                 plot.title = element_text(hjust = .5, size=20, colour="#535353"))
+  p <- p + scale_x_discrete(limits=c(1:max(data$degree))) # Scale x axis according to number of polynomials in the model
+  # Add solid black line to bottom of the plot (only needed if panel.border not set)
+  p <- p + geom_hline(yintercept=min(data$value) - 1,size=1,colour="#535353")
+
   return(p)
 }
--- plot_crossValidation.R
+++ plot_crossValidation.R
@@ -2,11 +2,27 @@
 
 plotCrossValidation <- function(seData){
   
-  p <- ggplot(data=seData, aes(x=degree, y=mse, colour=degree))
-  p <- p + geom_line()
+  p <- ggplot(data=seData, aes(x=degree, y=mse))
+  p <- p + geom_line(size=1.2, colour="#999999")
   p <- p + geom_errorbar(aes(x=degree, ymin=mse-se, ymax=mse+se), data = seData, width=0.25)
   p <- p + xlab("Degree of polynomial") + ylab("Mean Squared error")
   p <- p + ggtitle(label="Results of crossvalidation")
-  p <- p + theme(legend.position = "none")
-  p
+  # Set the entire chart region to a light gray color
+  p <- p + theme_bw()
+  p <- p + theme(panel.background=element_rect(fill="#F0F0F0"),
+                 plot.background=element_rect(fill="#F0F0F0"),
+                 panel.border=element_rect(colour="#F0F0F0"),
+                 #panel.border = element_rect(size=1.1, colour="#535353", fill=NA),
+                 panel.grid.major=element_line(colour="#D0D0D0",size=.5), # Format grid
+                 axis.ticks=element_blank(),
+                 axis.text.x = element_text(), # Drop y axis text, color axis titles, add plot border
+                 axis.text.y = element_blank(),
+                 axis.title = element_text(size=15, colour="#535353"),
+                 legend.position = "none", # Drop legend
+                 plot.title = element_text(hjust = .5, size=20, colour="#535353")) # Work title
+  p <- p + scale_x_discrete(limits=c(1:max(seData$degree)))
+  # Add solid black line to bottom of the plot (only needed if panel.border not set)
+  p <- p + geom_hline(yintercept=min(seData$mse) - 1, size=1, colour="#535353")
+  
+  return(p)
 }
